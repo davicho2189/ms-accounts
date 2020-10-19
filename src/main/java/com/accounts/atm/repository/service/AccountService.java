205bb34d71d2359b0e8772934b2530ba838eaaef
cesar david Guerrero Hidalgo <ce_david14@outlook.com>
2020-10-19 15:49:19 -0500

    account

--- src/main/java/com/accounts/atm/repository/service/AccountService.java
+++ src/main/java/com/accounts/atm/repository/service/AccountService.java
@@ -1,23 +1,20 @@
 package com.accounts.atm.repository.service;
 
 
-import com.accounts.atm.model.dto.AccountResponse;
-import com.accounts.atm.model.entity.Account;
-import com.accounts.atm.repository.dao.AccountDao;
-import io.reactivex.Single;
-import io.reactivex.schedulers.Schedulers;
-import lombok.extern.slf4j.Slf4j;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.accounts.atm.model.dto.AccountResponse;
+import com.accounts.atm.model.entity.Account;
+import com.accounts.atm.repository.dao.AccountDao;
 
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 
-@Slf4j
 @Service
 public class AccountService implements IAccountService {
 
@@ -30,14 +27,20 @@ public class AccountService implements IAccountService {
         List<Account> accounts =  accountDao
                 .findByCardNumber(document);
 
-        List<AccountResponse> responses = new ArrayList<>();
-        for (Account account: accounts) {
-            AccountResponse cardResponse = new AccountResponse();
-            BeanUtils.copyProperties(cardResponse, account);
-            responses.add(cardResponse);
-        }
-
-        responses.forEach(p->p.setAccountNumber(p.getAccountNumber().concat("xxxx")));
-        return Single.just(responses).subscribeOn(Schedulers.io());
+        List<AccountResponse> responses = new ArrayList<>();   
+        
+        //Thread.sleep(5000); return responses
+        return Single.just(responses)
+        		.map(response->{
+        			Thread.sleep(5000);
+        			for (Account account: accounts) {
+        	            AccountResponse cardResponse = new AccountResponse();
+        	            BeanUtils.copyProperties(cardResponse, account);
+        	            responses.add(cardResponse);
+        	        }
+        			responses.forEach(p->p.setAccountNumber(p.getAccountNumber().concat("xxxx")));
+        			return response;
+        		})
+        		.subscribeOn(Schedulers.io());
     }
 }
